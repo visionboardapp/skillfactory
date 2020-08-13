7bcacaf461d17033b0b9ff15d08109681f603c04
Olya <olya.yunakova@gmail.com>
2020-08-13 11:25:22 -0400

    add ViewController

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Cool
+//
+//  Created by OLYA on 6/13/20.
+//  Copyright © 2020 OLYA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
